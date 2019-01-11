b0522958806b063b0334200f3abdedf08aaea547
Kristopher Chayadi <kristopher.chayadi1688@gmail.com>
2019-01-11 15:14:35 +0700

    Adding HomePostDetail and HomePostDetailARSCN

---
+++ Picolo/Home/HomePostDetailARSCN.swift
@@ -0,0 +1,9 @@
+//
+//  HomePostDetailARSCN.swift
+//  Picolo
+//
+//  Created by Kristopher Chayadi on 11/01/19.
+//  Copyright © 2019 Ricky Adi Kuncoro. All rights reserved.
+//
+
+import Foundation
