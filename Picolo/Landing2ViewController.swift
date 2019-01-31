5980f040cc5659b2847c044bb4ab5c8e9dd15f41
Kristopher Chayadi <kristopher.chayadi1688@gmail.com>
2019-01-31 14:58:47 +0700

    Adding Landing Page Fixing Landing Page only shown if the application newly installed

---
+++ Picolo/Landing2ViewController.swift
@@ -0,0 +1,9 @@
+//
+//  Landing2ViewController.swift
+//  Picolo
+//
+//  Created by Kristopher Chayadi on 31/01/19.
+//  Copyright © 2019 Ricky Adi Kuncoro. All rights reserved.
+//
+
+import Foundation
