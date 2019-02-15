7c8d35d74957d9e4e3fd4f8a61633dd5c65c7877
Kristopher Chayadi <kristopher.chayadi1688@gmail.com>
2019-02-15 15:32:35 +0700

    cpmmit

--- Picolo/Home/HomePostDetailARSCN.swift
+++ Picolo/Home/HomePostDetailARSCN.swift
@@ -57,6 +57,42 @@ class HomePostDetailARSCN: UIViewController,ARSCNViewDelegate{
         return btn
     }()
     
+    let landingAR1: UIView = {
+        let uiv = UIView()
+        uiv.isHidden = true
+        uiv.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        return uiv
+    }()
+    
+    let landingAR2: UIView = {
+        let uiv = UIView()
+        uiv.isHidden = true
+        uiv.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        
+        return uiv
+    }()
+    
+    let toAR2Btn: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("Continue", for: .normal)
+        btn.addTarget(self, action: #selector(toAR2), for: .touchUpInside)
+        return btn
+    }()
+    let toAR1Btn: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("Back", for: .normal)
+        btn.addTarget(self, action: #selector(toAR1), for: .touchUpInside)
+        return btn
+    }()
+    let closeLandingAR: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("Let's Go", for: .normal)
+        btn.addTarget(self, action: #selector(closeLanding), for: .touchUpInside)
+        return btn
+    }()
+    
+    
+    
     var imagePost = CustomImageView()
     var image : UIImage?
     
@@ -70,26 +106,33 @@ class HomePostDetailARSCN: UIViewController,ARSCNViewDelegate{
     let configuration = ARWorldTrackingConfiguration()
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         sceneView.delegate = self
         view.addSubview(sceneView)
         view.addSubview(instructionLabel)
         view.addSubview(backButton)
         view.addSubview(resetButton)
+        view.addSubview(landingAR1)
+        view.addSubview(landingAR2)
+        landingAR1.addSubview(toAR2Btn)
+        landingAR2.addSubview(toAR1Btn)
+        landingAR2.addSubview(closeLandingAR)
         
         sceneView.setAnchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0)
         instructionLabel.setAnchor(top: view.safeAreaLayoutGuide.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 120, paddingLeft: 0, paddingBottom: 0, paddingRight: 0)
         instructionLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         backButton.setAnchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, bottom: nil, right: nil, paddingTop: 57, paddingLeft: 20, paddingBottom: 0, paddingRight: 0)
         resetButton.setAnchor(top: view.safeAreaLayoutGuide.topAnchor, left: nil, bottom: nil, right: view.rightAnchor, paddingTop: 57, paddingLeft: 0, paddingBottom: 0, paddingRight: 20)
-        
-        instructionLabel.text = "Detecting Plane"
-        flagplane = false
-        flagplace = false
-        
-        
-        sceneView.autoenablesDefaultLighting = true
-        
+        if !UserDefaults.standard.bool(forKey: "hasRunARBefore"){
+            landingAR1.isHidden = false
+        }
+        else {
+            instructionLabel.text = "Detecting Plane"
+            flagplane = false
+            flagplace = false
+            sceneView.autoenablesDefaultLighting = true
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -219,12 +262,24 @@ class HomePostDetailARSCN: UIViewController,ARSCNViewDelegate{
         }
         flagplane = false
         flagplace = false
-        
-        
     }
     
     @objc func back(){
         print("back kepencet")
         navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func toAR1(){
+        landingAR2.isHidden = true
+        landingAR1.isHidden = false
+    }
+    @objc func toAR2(){
+        landingAR1.isHidden = true
+        landingAR2.isHidden = false
+    }
+    @objc func closeLanding(){
+        landingAR1.isHidden = true
+        landingAR2.isHidden = true
+        UserDefaults.standard.set("true", forKey: "hasRunARBefore")
     }
 }
